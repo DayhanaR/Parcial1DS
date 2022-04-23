1bb1a3501fc7b3892874d8a59169336c42a9d330
DayhanaRamirez <dayhanaramirez45@gmail.com>
2022-04-23 02:44:55 -0500

    Finished exam.

---
+++ Parcial1/Migrations/20220423074052_ChangedEntraceEntity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parcial1.Migrations
+{
+    public partial class ChangedEntraceEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Parcial1/Models/TicketViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1.Models
+{
+    public class TicketViewModel
+    {
+        [Display(Name = "ID de la boleta")]
+        [MaxLength(4, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Id { get; set; }
+    }
+}
